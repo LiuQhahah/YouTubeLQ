f1c509d0845e5ad239c8d8bb61d851bd912105fc
liu <1192395260@qq.com>
2018-03-01 17:11:00 +0800

    修改kotlin的bug错误

---
+++ Readme.md
@@ -0,0 +1,29 @@
+#学习youtube
+
+视频链接https://www.youtube.com/watch?v=jS0buQyfJfs&list=PL0dzCUj1L5JGfHj1lwxOq67zAJV3e1S9S&index=1
+
+#01介绍RecyclerView
+
+
+##bug:unsolve
+
+    import kotlinx.android.synthetic.main.activity_main.*
+
+[参考链接](https://stackoverflow.com/questions/34169562/unresolved-reference-kotlinx)
+
+
+
+1. 在项目工程的 build.gradle中
+
+
+        buildscript {
+        dependencies {
+            classpath "org.jetbrains.kotlin:kotlin-android-extensions:$kotlin_version"
+        }
+        }
+
+2. 在模块级别的build.gralde中
+
+        apply plugin: 'kotlin-android-extensions'
+
+**注**window不存在，ubuntu存在这个错误
---
+++ app/src/main/java/com/liuqhahah/youtubelq/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.liuqhahah.youtubelq
+
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        recyclerView_main.layoutManager = LinearLayoutManager(this)
+
+
+
+
+    }
+}
